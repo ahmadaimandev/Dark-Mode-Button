c06525689c23640388be64e3e1bbaefb85aa6f61
Ahmad-Aiman <aimanzulbahari@gmail.com>
2023-04-07 22:58:31 +0800

    Update all the button.

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-const checkbox = document.getElementById('checkbox');
+const body = document.querySelector('body');
+const btn = document.querySelector('.btn');
+const icon = document.querySelector('.btn_icon');
 
-checkbox.addEventListener('change', ()=>{
-  document.body.classList.toggle('dark');
+btn.addEventListener('click', () => {
+
+  body.classList.toggle('darkmode');
+  icon.classList.add('animated');
+
+  if(body.classList.contains('darkmode')) {
+    icon.classList.remove('fa-sun');
+    icon.classList.add('fa-moon');
+  }
+  else {
+    icon.classList.remove('fa-moon');
+    icon.classList.add('fa-sun');
+  }
 })
